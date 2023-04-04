46ef8389f34aecc5e8bf26c31be4f657e42caecd
Erico Couto Jr <erico.coutojr@gmail.com>
2023-04-04 19:21:58 -0300

    Exercício 02

--- src/components/DadosPessoaisForm.jsx
+++ src/components/DadosPessoaisForm.jsx
@@ -1,8 +1,8 @@
 function DadosPessoaisForm() {
   return (
-    <div className="container container-md mt-5">
+    <div className="container container-md my-5">
       <div className="mb-3">
-        <label for="formNomeInput" className="form-label">
+        <label htmlFor="formNomeInput" className="form-label">
           Nome
         </label>
         <input
@@ -13,12 +13,12 @@ function DadosPessoaisForm() {
         />
       </div>
       <div className="mb-3">
-        <label for="formEmailInput" class="form-label">
+        <label htmlFor="formEmailInput" className="form-label">
           E-mail
         </label>
         <input
           type="email"
-          class="form-control"
+          className="form-control"
           id="formEmailInput"
           placeholder="Digite seu e-mail..."
         />
